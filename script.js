efb22f7e7d57a03e66cda8cd9c6510184cc0886d
GwenaRoux <62894847+GwenaRoux@users.noreply.github.com>
2020-07-10 13:29:25 +0200

    Commited
    yes

---
+++ script.js
@@ -0,0 +1,27 @@
+function fetchData() {
+    fetch("http://91.121.88.38:3000/api/people")
+        .then(response => {
+            if (!response.ok) {
+                throw Error("Marche po !");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const html = data.map(user => {
+
+                    for (const dataKey in data) {
+                        return `<div class="user">
+                                <p> Nom : ${user.name}</p>
+                                <p> Age : ${user.age}</p>
+                                 <p> Voitures : </p>
+                                        <p>${user.cars}</p>
+                             </div>
+                            <hr>`;
+                    }
+                })
+                .join("");
+            document.querySelector("#liste").insertAdjacentHTML("afterbegin", html);
+        })
+}
+
+fetchData();
